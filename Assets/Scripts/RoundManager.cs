28435b99db5ed4f4a42d17e5295bb702b2841405
Brandon Cooper <brandonkoopa@hotmail.com>
2020-02-01 23:29:55 -0600

    carts & animation (#3)

--- Assets/Scripts/RoundManager.cs
+++ Assets/Scripts/RoundManager.cs
@@ -10,6 +10,13 @@ public class RoundManager : MonoBehaviour
   public List<RoundDescriptor> Rounds;
   private int currentRound = -1;
 
+  public Animator cartsAnimator;
+  private int endRoundTrigger = Animator.StringToHash("EndRound");
+
+
+  public GameObject leftRobotHolder;
+  public GameObject rightRobotHolder;
+
   // Start is called before the first frame update
   void Start()
   {
@@ -17,6 +24,11 @@ public class RoundManager : MonoBehaviour
     if (occilliscopeManager == null)
     {
       Debug.LogError("No Occilliscope Manager configured");
+    }
+
+    if (cartsAnimator == null)
+    {
+      Debug.LogError("No animator for round manager");
     }
   }
 
@@ -40,11 +52,12 @@ public class RoundManager : MonoBehaviour
       currentRound = 0;
     }
     // TODO update the spawn location
-    StartRound(Instantiate(Rounds[currentRound].leftRobotPrefab), Instantiate(Rounds[currentRound].rightRobotPrefab));
+    StartRound(Instantiate(Rounds[currentRound].leftRobotPrefab, leftRobotHolder.transform), Instantiate(Rounds[currentRound].rightRobotPrefab, rightRobotHolder.transform));
   }
 
   public void StartRound(Robot leftRobot, Robot rightRobot)
   {
+    cartsAnimator.SetTrigger(endRoundTrigger);
     if (occilliscopeManager)
     {
       occilliscopeManager.ConnectRobots(leftRobot, rightRobot);
