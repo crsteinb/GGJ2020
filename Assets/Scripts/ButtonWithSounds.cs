1220ab91910934af3b8ede0039cfcf65a0d3b782
Brandon Cooper <bcooper@predictivescience.com>
2020-02-02 12:37:23 -0600

    button sounds

---
+++ Assets/Scripts/ButtonWithSounds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonWithSounds : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	public string pressSoundName = "TouchOut";
+	public string unpressSoundName = "TouchIn";
+
+	public void OnPointerDown(PointerEventData d) {
+		FindObjectOfType<AudioManager>().Play(pressSoundName);
+	}
+
+	public void OnPointerUp(PointerEventData d) {
+		FindObjectOfType<AudioManager>().Play(unpressSoundName);
+	}
+}
