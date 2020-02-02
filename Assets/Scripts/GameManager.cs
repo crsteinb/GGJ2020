0d9cb8d5e6f42239cafca9761a68d109e07af0d3
Brandon Cooper <bcooper@predictivescience.com>
2020-02-01 23:27:34 -0600

    carts & animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public PartsDropper partsDropper;
+    public RoundManager roundManager;
 
     // Start is called before the first frame update
     void Start() {
         // drop first part
+        StartGame(); 
+    }
+
+    void StartGame() {
         partsDropper.drop();
+        roundManager.StartNextRound();
     }
 }
